30383205cc23750046de0a70106c93cfc30a2740
Julian <crjuliantico>
2019-08-02 00:41:29 -0600

    mas

---
+++ XamarinAppExpleShop/XamarinAppExpleShop.WebAPI/Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace XamarinAppExpleShop.Web.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using XamarinAppExpleShop.Web.Data.Entities;
+
+    public class DataContext:DbContext
+    {
+
+        public DbSet<Product> Products { get; set; }
+
+
+        public DataContext( DbContextOptions<DataContext> options)
+            : base(options)
+        {
+
+        }
+
+    }
+}
