52f8988e4a8f4cd7e9e189c1b71415f92426d867
Julian <crjuliantico>
2019-08-06 00:42:01 -0600

    Hasta video 13 API

---
+++ XamarinAppExpleShop/XamarinAppExpleShop.WebAPI/Controllers/API/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XamarinAppExpleShop.Web.Controllers.API
+{
+
+    using Data;
+    using Microsoft.AspNetCore.Mvc;
+
+
+    [Route("api/[Controller]")]
+    public class ProductsController: Controller
+    {
+        private readonly IProductRepository productRepository;
+
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(this.productRepository.GetAll());
+        }
+
+
+
+    }
+
+
+
+
+}
