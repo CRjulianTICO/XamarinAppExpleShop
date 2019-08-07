709777b0b705df1b67704f1caa6bc2e2f0160f02
Julian <crjuliantico>
2019-08-06 23:59:45 -0600

    Hasta el tutorial 15

---
+++ XamarinAppExpleShop/XamarinAppExpleShop.WebAPI/Models/ProductViewModel.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace XamarinAppExpleShop.Web.Models
+{
+
+    using Microsoft.AspNetCore.Http;
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using XamarinAppExpleShop.Web.Data.Entities;
+    public class ProductViewModel:Product
+    {
+        [Display(Name="Image")]
+        public IFormFile ImageFile { get; set; }
+    }
+}
